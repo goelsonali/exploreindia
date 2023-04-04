56100c53b90165e8072666159e4357a8a76ddc9d
Sonali Goel <112942761+goelsonali@users.noreply.github.com>
2023-04-04 12:10:02 +0100

    Initial commit

---
+++ src/test/java/com/explore/ec/ExploreindiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explore.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreindiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
